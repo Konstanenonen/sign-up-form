0946ec1a9920cf221629ba3bc31849d5faf331fe
Konsta Nenonen <konstanenonen@protonmail.com>
2022-04-03 22:31:03 +0300

    Added script to check matching passwords, adde media screens to mobile, tablet and big screens

---
+++ index.js
@@ -0,0 +1,30 @@
+function checkPasswordMatch(event) {
+  event.preventDefault();
+  const firstPassword = document.getElementById("password");
+  const secondPassword = document.getElementById("confirm-password");
+  console.log(`first = ${firstPassword}, second = ${secondPassword}`);
+
+  if (firstPassword.value !== secondPassword.value) {
+    firstPassword.classList.add("error");
+    secondPassword.classList.add("error");
+
+    const errorText = "*Passwords do not match";
+    const errorElement = document.createElement("p");
+    errorElement.innerText = errorText;
+    errorElement.classList.add("error-text");
+    document.getElementById("password-field").appendChild(errorElement);
+
+    return;
+  }
+
+  const errorText = document.querySelector(".error-text");
+  document.getElementById("password-field").removeChild(errorText);
+  firstPassword.classList.remove("error");
+  secondPassword.classList.remove("error");
+
+  const inputFields = document.querySelectorAll("input");
+
+  inputFields.forEach(input => input.value = "");
+}
+
+document.getElementById("account-form").addEventListener("submit", checkPasswordMatch);
